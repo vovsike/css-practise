b980ef316651ef916786d9d3733c87d4ad1247a0
Vladimir Garanin <vladimir.garanin2802@gmail.com>
2025-01-28 23:14:00 +0000

    init commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,37 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <>
+          <nav>
+              <span>Some text</span>
+              <span>Some text</span>
+              <span>Some text</span>
+              <span>Some text</span>
+          </nav>
+          <div className="app">
+              <form className={"container login-form"}>
+                  <span className={"form-title"}>Please login</span>
+                  <hr className={"hr"}/>
+                  <label className={"form-label"}>
+                      Username
+                      <input className={"form-input"} type={"text"}/>
+                  </label>
+                  <label className={"form-label"}>
+                      Password
+                      <input className={"form-input"} type={"password"}/>
+                      <a href={"https://google.com"} className={"forgot-password"}>Forgot password</a>
+                  </label>
+                  <div className={"flex-horizontal"}>
+                      <button className={"button lgoin-button"} type="submit">Login</button>
+                      <button className={"button signup-button"} type="submit">Sign up</button>
+                  </div>
+              </form>
+          </div>
+      </>
+
+  )
+}
+
+export default App
