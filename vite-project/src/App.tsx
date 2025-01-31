e4e0789b4d28e4b33c2906a7989c3562553a4c46
Vladimir Garanin <vladimir.garanin2802@gmail.com>
2025-01-31 09:46:12 +0000

    more changes

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,36 +2,40 @@ import './App.css'
 
 function App() {
 
-  return (
-      <div className={"app"}>
-          <nav className={"navbar"}>
-              <span>Some text</span>
-              <span>Some text</span>
-              <span>Some text</span>
-              <span>Some text</span>
-          </nav>
-          <div className="main">
-              <form className={"container login-form"}>
-                  <span className={"form-title"}>Please login</span>
-                  <hr className={"hr"}/>
-                  <label className={"form-label"}>
-                      Username
-                      <input className={"form-input"} type={"text"}/>
-                  </label>
-                  <label className={"form-label"}>
-                      Password
-                      <input className={"form-input"} type={"password"}/>
-                      <a href={"https://google.com"} className={"forgot-password"}>Forgot password</a>
-                  </label>
-                  <div className={"flex-horizontal"}>
-                      <button className={"button lgoin-button"} type="submit">Login</button>
-                      <button className={"button signup-button"} type="submit">Sign up</button>
-                  </div>
-              </form>
-          </div>
-      </div>
+    return (
+        <div className={"app"}>
+            <nav className={"navbar"}>
+                <span>Some text</span>
+                <span>Some text</span>
+                <span>Some text</span>
+                <span>Some text</span>
+            </nav>
+            <div className="main">
+                <form className={"container login-form"}>
+                    <span className={"form-title"}>Please login</span>
+                    <hr className={"hr"}/>
+                    <div className={"form-control"}>
+                        <label className={"form-label"}>
+                            Username
+                            <input size={35} className={"form-input"} type={"text"}/>
+                        </label>
+                    </div>
+                    <div className={"form-control"}>
+                        <label className={"form-label"}>
+                            Password
+                            <input size={35} className={"form-input"} type={"password"}/>
+                            <a href={"https://google.com"} className={"forgot-password"}>Forgot password</a>
+                        </label>
+                    </div>
+                    <div className={"flex-horizontal"}>
+                        <button className={"button lgoin-button"} type="submit">Login</button>
+                        <button className={"button signup-button"} type="submit">Sign up</button>
+                    </div>
+                </form>
+            </div>
+        </div>
 
-  )
+    )
 }
 
 export default App
